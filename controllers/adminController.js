4a23e87464fc6d94252430c6a387fb35e6baf10f
Cyruz Aguilar <aguilarcyruzjaeg@gmail.com>
2025-10-11 14:10:30 +0800

    updates

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const Product = require('../models/Product');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Order = require('../models/Order');
 const Staff = require('../models/Staff');
 const CompletedOrder = require('../models/CompletedOrder');
